dae1c7265ef21d7d84076456882d0c9a4d4cf528
stan435 <stan.baeckelandt@telenet.be>
2024-05-14 13:05:55 +0200

    frontend setup

---
+++ frontend-yosto/android/app/src/main/java/com/yosto/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.yosto.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend-yosto/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yosto.app',
+  appName: 'frontend-yosto',
+  webDir: 'dist/frontend-yosto/browser'
+};
+
+export default config;
