41028b7332db50caa94f02a56124be85a9b1a9b4
Wietse-Gijbels <wietse.gijbels@student.ucll.be>
2024-05-15 15:42:36 +0200

    components created stomp and sockjs imported

---
+++ src/app/chat/chat.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import SockJS from 'sockjs-node';
+import {Stomp} from "@stomp/stompjs";
+
+@Component({
+  selector: 'app-chat',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat.component.html',
+  styleUrl: './chat.component.scss'
+})
+export class ChatComponent {
+
+
+}
