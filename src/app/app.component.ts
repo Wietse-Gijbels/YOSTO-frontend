fdd5f45fd26b0cb92b8774d872eaa18c37f01cd6
kunovercammen <kuno.vercammen@student.ucll.be>
2024-05-15 12:56:54 +0200

    Email ophalen vanuit backend en displayen op frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {BackendConnectionComponent} from "../backend-connection/backend-connection.component";
+import {HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BackendConnectionComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/service/backend-connection.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {TestResponse} from "../app/models/test-response";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackendConnectionService {
+
+  private testUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.testUrl = 'http://localhost:8080/api/v1/gebruiker';
+  }
+
+  getUserByEmail(email: string): Observable<any> {
+    const url = `${this.testUrl}/${email}`;
+    return this.http.get<any>(url);
+  }
+
+}
