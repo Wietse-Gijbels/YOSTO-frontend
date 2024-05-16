5a4cc6f9106047bb80885bb30d2df1db047d4557
stan435 <stan.baeckelandt@telenet.be>
2024-05-16 16:12:12 +0200

    geen fa-icons

--- src/app/navigation/nav-bar.component.ts
+++ src/app/navigation/nav-bar.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-import {FaIconComponent} from "@fortawesome/angular-fontawesome";
-import {faClipboardQuestion, faGraduationCap, faHouse} from "@fortawesome/free-solid-svg-icons";
-import {faHeart} from "@fortawesome/free-solid-svg-icons/faHeart";
-import {RouterLink, RouterLinkActive} from "@angular/router";
-import {faComment} from "@fortawesome/free-regular-svg-icons";
-import {NgOptimizedImage} from "@angular/common";
-
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
@@ -14,10 +10,4 @@ import {NgOptimizedImage} from "@angular/common";
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
 })
-export class NavBarComponent {
-  protected readonly faGraduationCap = faGraduationCap;
-  protected readonly faHeart = faHeart;
-  protected readonly faClipboardQuestion = faClipboardQuestion;
-  protected readonly faComment = faComment;
-  protected readonly faHouse = faHouse;
-}
+export class NavBarComponent {}
