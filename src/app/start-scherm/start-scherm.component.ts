ea2cf89bb179ef6c8196f391ec70ce221da5c97a
kunovercammen <kuno.vercammen@student.ucll.be>
2024-05-16 11:53:06 +0200

    Opmaak startscherm klaar

---
+++ src/app/start-scherm/start-scherm.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-scherm',
+  standalone: true,
+  imports: [],
+  templateUrl: './start-scherm.component.html',
+  styleUrl: './start-scherm.component.scss'
+})
+export class StartSchermComponent {
+
+}
