1ba2167f5eade0ca9c3f6cc2924877b304a461bc
stan435 <stan.baeckelandt@telenet.be>
2024-05-16 16:00:35 +0200

    nav bar gebruikt png, stylguide in nav-bar.component.scss

--- src/app/navigation/nav-bar.component.ts
+++ src/app/navigation/nav-bar.component.ts
@@ -4,21 +4,17 @@ import {faClipboardQuestion, faGraduationCap, faHouse} from "@fortawesome/free-s
 import {faHeart} from "@fortawesome/free-solid-svg-icons/faHeart";
 import {RouterLink, RouterLinkActive} from "@angular/router";
 import {faComment} from "@fortawesome/free-regular-svg-icons";
+import {NgOptimizedImage} from "@angular/common";
 
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [
-    FaIconComponent,
-    RouterLink,
-    RouterLinkActive
-  ],
+  imports: [FaIconComponent, RouterLink, RouterLinkActive, NgOptimizedImage],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrl: './nav-bar.component.scss',
 })
 export class NavBarComponent {
-
   protected readonly faGraduationCap = faGraduationCap;
   protected readonly faHeart = faHeart;
   protected readonly faClipboardQuestion = faClipboardQuestion;
