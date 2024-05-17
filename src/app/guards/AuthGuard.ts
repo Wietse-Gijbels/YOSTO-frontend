a8cbad942d835a5651f630b0070a439772ddcde8
Ube-VanGrimbergen <ube.vangrimbergen@student.ucll.be>
2024-05-17 09:53:52 +0200

    prettier test

--- src/app/guards/AuthGuard.ts
+++ src/app/guards/AuthGuard.ts
@@ -6,9 +6,7 @@ export const authGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (authService.isLoggedIn()) {
-    return true;
-  } else {
+  if (authService.isLoggedIn()) {return true;} else {
     router.navigateByUrl('/login');
     return false;
   }
