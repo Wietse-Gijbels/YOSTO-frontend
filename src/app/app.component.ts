72e71a32eb8bf5df6d8462d92bf76245a9e6b511
Ube-VanGrimbergen <ube.vangrimbergen@student.ucll.be>
2024-05-19 15:47:34 +0200

    localstorage merde

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,36 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
-import {BackendConnectionComponent} from "../backend-connection/backend-connection.component";
-import {HttpClientModule} from "@angular/common/http";
-import {RegistreerComponent} from "./gebruiker/registreer/registreer.component";
-import {AuthService} from "./service/auth.service";
-import {NgIf} from "@angular/common";
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { BackendConnectionComponent } from '../backend-connection/backend-connection.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RegistreerComponent } from './gebruiker/registreer/registreer.component';
+import { AuthService } from './service/auth.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BackendConnectionComponent, HttpClientModule, RouterLink, RegistreerComponent, NgIf],
+  imports: [
+    RouterOutlet,
+    BackendConnectionComponent,
+    HttpClientModule,
+    RouterLink,
+    RegistreerComponent,
+    NgIf,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-
   curr: any;
-
-  constructor(
-    private authService: AuthService,
-  ) {
-  }
+  token: string | null = '';
+  constructor(private authService: AuthService) {}
   ngOnInit(): void {
+    if (localStorage.getItem('token')) {
+      this.token = localStorage.getItem('token');
+    }
   }
 
   Logout(): void {
-
+    localStorage.removeItem('token');
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
-import {LoginComponent} from "./gebruiker/login/login.component";
-import {RegistreerComponent} from "./gebruiker/registreer/registreer.component";
-import {ChatComponent} from "./chat/chat.component";
+import { LoginComponent } from './gebruiker/login/login.component';
+import { RegistreerComponent } from './gebruiker/registreer/registreer.component';
+import { ChatComponent } from './chat/chat.component';
 
-export const routes: Routes = [{
-  path: '',
-  children:[
-    {path: 'login',component: LoginComponent},
-    {path: 'registreer',component: RegistreerComponent},
-    {path: '', component: ChatComponent}
-  ]
-}];
+export const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'registreer', component: RegistreerComponent },
+      { path: 'chat', component: ChatComponent },
+    ],
+  },
+];
