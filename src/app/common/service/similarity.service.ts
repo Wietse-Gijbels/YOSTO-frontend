45c6e6f8b3ee8e6d774d4e676ec6686c3e3299b0
Ube-VanGrimbergen <ube.vangrimbergen@student.ucll.be>
2024-06-02 13:31:11 +0200

    merge met kuno

--- src/app/common/service/similarity.service.ts
+++ src/app/common/service/similarity.service.ts
@@ -29,8 +29,7 @@ interface StudierichtingSimilarityDto {
   providedIn: 'root',
 })
 export class SimilarityService {
-  private apiUrl =
-    'http://localhost:8080/api/v1/studierichtingWaardes';
+  private apiUrl = 'http://localhost:8080/api/v1/studierichtingWaardes';
 
   constructor(private http: HttpClient) {}
 
